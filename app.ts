d8a11a93161ccce31cd59639afc9b0cd9960246a
PatrykZE <zegarowiczpatryk@gmail.com>
2022-04-25 23:21:12 +0200

    List all files

--- app.ts
+++ app.ts
@@ -1,2 +1,21 @@
+const fs = require('fs');
 
+const logFolder = 'Z:/Projekty/logs/m002/2022_mar';
+
+function GetFiles(Location:string){
+    return new Promise(function(resolve, reject) {
+    fs.readdir(Location, (err: ErrorConstructor, files: string[]) => {
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        }
+        else {
+            resolve(files);
+            console.log(files);
+        }
+    });
+});
+    
+}
+
+GetFiles(logFolder); 
 
