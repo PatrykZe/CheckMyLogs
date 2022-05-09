8db84a231975c93afca9e89ff090da5e5e6ec750
PatrykZE <zegarowiczpatryk@gmail.com>
2022-05-10 00:24:06 +0200

    Async Read DIR

--- app.ts
+++ app.ts
@@ -1,21 +1,30 @@
 const fs = require('fs');
-
+const fsPromises = fs.promises;
 const logFolder = 'Z:/Projekty/logs/m002/2022_mar';
 
-function GetFiles(Location:string){
-    return new Promise(function(resolve, reject) {
-    fs.readdir(Location, (err: ErrorConstructor, files: string[]) => {
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        }
-        else {
-            resolve(files);
-            console.log(files);
-        }
-    });
-});
-    
+
+async function listDir(path: string) {
+  try {
+    const DIR = fsPromises.readdir(path);
+    return DIR;
+  } catch (err) {
+    console.error('Error occured while reading directory!', err);
+  }
 }
 
-GetFiles(logFolder); 
+async function OpenLog(LogPath:string) {
+  const Log = fs.readFileSync(LogPath,{encoding:'utf8', flag:'r'});
+  return Log; 
+}
+
+async function ReadLogs(){
+  const ABC = await listDir(logFolder); 
+  const logName = ABC[0];
+  const LogPath = `${logFolder}/${logName}`;
+  const LogData = await OpenLog(LogPath);
+  console.log(LogData);
+}
+
+
+ReadLogs();
 
