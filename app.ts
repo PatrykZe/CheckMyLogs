52311f05076937e6fc14388c90075a4289737fdd
PatrykZE <zegarowiczpatryk@gmail.com>
2022-05-23 23:12:45 +0200

    ChoseLogFunction

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ const fsPromises = fs.promises;
 const logFolder = 'Z:/Projekty/logs/m002/2022_mar';
 
 
-async function listDir(path: string) {
+async function ListDir(path: string) {
   try {
     const DIR = fsPromises.readdir(path);
     return DIR;
@@ -13,16 +13,34 @@ async function listDir(path: string) {
 }
 
 async function OpenLog(LogPath:string) {
+  try {
   const Log = fs.readFileSync(LogPath,{encoding:'utf8', flag:'r'});
-  return Log; 
+  return Log;
+  } catch (err) {
+    console.error(`Error occured while reading single log ${LogPath}!`, err);
+  }
 }
 
+
+async function ChoseLog(logs:object) {
+  
+  for (const log in logs) {
+    const LogData = await OpenLog(log);
+    console.log(LogData);
+  }
+}
+
+
+
+
+
 async function ReadLogs(){
-  const ABC = await listDir(logFolder); 
-  const logName = ABC[0];
-  const LogPath = `${logFolder}/${logName}`;
-  const LogData = await OpenLog(LogPath);
-  console.log(LogData);
+  const logs_array = await ListDir(logFolder);  //Lista ze wszystkimi logami
+  const abc = await ChoseLog(logs_array);
+  // const logName = logs_array[0];                // TEST
+  // const LogPath = `${logFolder}/${logName}`;   //choose single log 
+  // const LogData = await OpenLog(LogPath);      //open single log
+  // console.log(LogData);
 }
 
 
