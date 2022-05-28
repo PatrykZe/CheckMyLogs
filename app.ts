01371747fe82000a2b634ed0bc5d1e336e7da549
PatrykZE <zegarowiczpatryk@gmail.com>
2022-05-28 14:32:44 +0200

    Search in all files

--- app.ts
+++ app.ts
@@ -13,32 +13,58 @@ async function ListDir(path: string) {
 }
 
 async function OpenLog(LogPath:string) {
+  
+  const regEx = new RegExp("lte", "i");
+  const result: string[] = [];
+  
   try {
-  const Log = fs.readFileSync(LogPath,{encoding:'utf8', flag:'r'});
-  return Log;
-  } catch (err) {
+    fs.readFile(LogPath,'utf8', function (err:Error, contents:[]) {
+  
+    // console.log(contents, typeof(contents));
+      let lines:string[] = contents.toString().split("\n");
+            lines.forEach(line => {
+                if (line && line.search(regEx) >= 0) {
+                    console.log('found in file ', line)
+                    result.push(line)
+                }
+            })
+            console.log('finished search');
+            return result;
+  
+  })} 
+  catch (err) {
     console.error(`Error occured while reading single log ${LogPath}!`, err);
   }
 }
 
 
-async function ChoseLog(logs:object) {
+// async function ChoseLog(logs:object) {
   
-  for (const log in logs) {
-    const LogData = await OpenLog(log);
-    console.log(LogData);
-  }
-}
+//   for (const log in logs) {
+//     const LogData = await OpenLog();
+//     console.log(LogData);
+//   }
+// }
 
 
 
 
 
 async function ReadLogs(){
-  const logs_array = await ListDir(logFolder);  //Lista ze wszystkimi logami
-  const abc = await ChoseLog(logs_array);
-  // const logName = logs_array[0];                // TEST
-  // const LogPath = `${logFolder}/${logName}`;   //choose single log 
+  const logs_object = await ListDir(logFolder);        //Lista ze wszystkimi logami
+  const logs_array = Object.values(logs_object); 
+  const Result = await logs_array.forEach((keys) => {OpenLog(`${logFolder}/${keys}`)});
+
+
+  // const LogPath = `${logFolder}/${logs_array[0]}`;    //Pierwszy log - Path
+  // const Result = await OpenLog(LogPath);
+  // console.log(Result);
+  
+
+
+  // const abc = await ChoseLog(logs_array);
+  // const logName = logs_array[0];                // Nazwa pierwszego pliku z katalogu
+  //   //choose single log 
   // const LogData = await OpenLog(LogPath);      //open single log
   // console.log(LogData);
 }
